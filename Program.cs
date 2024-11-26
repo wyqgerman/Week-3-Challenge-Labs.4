e40d8fef4afb87e86121258b22034faf3fcf9b03
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-11-26 09:25:27 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Week_3_Challenge_Labs._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinimumLength("ABFCACDB")); 
+            Console.WriteLine(MinimumLength("ACBBD"));
+        }
+
+        static int MinimumLength(string s)
+        {
+            bool foundSubstrings = true;
+
+            while (foundSubstrings)
+            {
+                string newString = s.Replace("AB", "").Replace("CD", "");
+
+                if (newString == s)
+                {
+                    foundSubstrings = false;
+                }
+                else
+                {
+                    s = newString;
+                }
+            }
+
+            return s.Length;
+        }
+    }
+}
